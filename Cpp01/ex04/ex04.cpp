05f3e230f3212aed7d483075517bf3e7d2cea65b
Daniele Ridolfo <shadan@Shadan>
2022-12-20 15:44:53 +0100

    ex04 almost done

---
+++ Cpp01/ex04/ex04.cpp
@@ -0,0 +1,89 @@
+#include "mysed.hpp"
+
+static std::string	getFile(std::string path)
+{
+	std::ifstream	f;
+	std::string		buff;
+	std::string		res;
+
+	f.open(path);
+	if (!f) 
+	{
+		std::cout << "No such file";
+		res = "";
+	}
+	else 
+	{
+		while (std::getline(f, buff)) 
+		{
+			if (f.eof())
+				break;
+			res += buff;
+			res += "\n";
+		}
+		res += buff;
+	}
+	f.close();
+	return (res);
+}
+
+static std::string	freplace(std::string str, std::string src, std::string pst)
+{
+	std::string	res;
+	std::string buff;
+
+	for (int x=0; x <= str.size(); x++)
+	{
+		if (str[x] == src[0])
+		{
+			buff = str.substr(x, src.size());
+			if (!buff.compare(src))
+			{
+				res += pst;
+				x += src.size() - 1;
+			}
+		}
+		else
+			res += str[x];
+	}
+	return (res);
+}
+
+static int	putFile(std::string path, std::string str)
+{
+	std::ofstream out(path);
+
+	if (!out)
+		return (1);
+	out << str;
+	out.close();
+	return (0);
+}
+
+int mySed(std::string path, std::string s1, std::string s2)
+{
+	std::string	res;
+
+	res = getFile(path);
+	
+	if (res != "")
+		res = freplace(res, s1, s2);
+	else
+		return (1);
+	
+	putFile(path.append(".replace"), res);
+	std::cout << "Done";
+	return (0);
+}
+
+int main(int argc, char **argv)
+{
+	
+	if (argc != 4)
+		std::cout << "Not enough params";
+	else
+		mySed(argv[1], argv[2], argv[3]);
+
+	std::cout << std::endl;
+	return (0);
+}
---
+++ Cpp01/ex04/mysed.hpp
@@ -0,0 +1,18 @@
+#ifndef MYSED_HPP
+# define MYSED_HPP
+
+# include <iostream>
+# include <iomanip>
+# include <cstring>
+# include <fstream>
+
+# define RED	"\033[91m"
+# define GREEN "\033[92m"
+# define YELLOW "\033[93m"
+# define BLUE "\033[94m"
+# define MAGENTA "\033[95m"
+# define BLANK "\033[0m"
+# define RL_START "\1"
+# define RL_END "\2"
+
+#endif
