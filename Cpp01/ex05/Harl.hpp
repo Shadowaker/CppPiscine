9c23ffe53386e23695b10a5890d5dc6479c922e9
Daniele Ridolfo <shadan@Shadan>
2022-12-21 12:39:39 +0100

    ex05 done

---
+++ Cpp01/ex05/Harl.hpp
@@ -0,0 +1,33 @@
+#ifndef HARL_HPP
+# define HARL_HPP
+
+# include <iostream>
+# include <iomanip>
+# include <cstring>
+# include <fstream>
+
+# define RED	"\033[91m"
+# define GREEN "\033[92m"
+# define YELLOW "\033[93m"
+# define BLUE "\033[94m"
+# define MAGENTA "\033[95m"
+# define BLANK "\033[0m"
+# define RL_START "\1"
+# define RL_END "\2"
+
+class Harl
+{
+	private:
+		void	debug(void);
+		void	info(void);
+		void	warning(void);
+		void	error(void);	
+
+	public:
+		Harl();
+		~Harl();
+	
+	void	complain(std::string level);
+};
+
+#endif
---
+++ Cpp01/ex05/harl.cpp
@@ -0,0 +1,52 @@
+#include "Harl.hpp"
+
+Harl::Harl()
+{
+	;
+}
+
+Harl::~Harl()
+{
+	;
+}
+
+void	Harl::debug(void)
+{
+	std::cout << "I love having extra bacon for my "
+		"7XL-double-cheese-triple-pickle-specialketchup burger.\nI really do!" << std::endl;
+}
+
+void	Harl::info(void)
+{
+	std::cout << "I cannot believe adding extra bacon costs more money.\n"
+		"You didn't put enough bacon in my burger!\nIf you did, I wouldn't be asking for more!" << std::endl;
+}
+
+void	Harl::warning(void)
+{
+	std::cout << "I think I deserve to have some extra bacon for free.\n"
+		"I've been coming for years whereas you started working here since last month." << std::endl;
+}
+
+void	Harl::error(void)
+{
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void	Harl::complain(std::string level)
+{
+	void	(Harl::*complain[4])(void) = 
+	{
+		&Harl::debug,
+		&Harl::info,
+		&Harl::warning,
+		&Harl::error
+	};
+	std::string	levels[4] = {"debug", "info", "warning", "error"};
+
+	for (int x = 0; x < 4; x++)
+	{
+		if (level == levels[x])
+			(this->*complain[x])();
+	}
+}
