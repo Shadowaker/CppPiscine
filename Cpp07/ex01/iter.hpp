fe98340731ac3ee89f13b0cde9f0d14cbea6a9b3
Daniele Ridolfo <dridolfo@e3r5p1.42roma.it>
2023-01-14 15:08:55 +0100

    Cpp07 done

---
+++ Cpp07/ex01/iter.hpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+template <typename T>
+void iter(T* array, size_t length, void (*f)(const T&))
+{
+	for (size_t i = 0; i < length; i++)
+		(*f)(array[i]);
+}
+
+template <typename T>
+void print_cose(const T& c)
+{
+	std::cout << c << std::endl;
+}
---
+++ Cpp07/ex02/Array.hpp
@@ -0,0 +1,69 @@
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+# include <string>
+# include <iostream>
+
+template <typename T>
+class Array
+{
+	private:
+		unsigned int	size;
+		T*				array;
+
+	protected:
+		;
+
+	public:
+		Array() : size(0), array(new T[0]) {};
+		Array(unsigned int n) : size(n), array(new T[n]) {};
+		Array(const Array& arr)
+		{
+			this->array = new T[arr.size];
+			this->size = arr.size;
+			for (unsigned int x = 0; x < arr.size; x++)
+				this->array[x] = arr.array[x];
+		}
+
+		~Array()
+		{
+			delete[] this->array;
+		}
+
+	Array& operator=(const Array& arr)
+	{
+		if (this != &arr)
+		{
+			this->~Array();
+			this->size = arr.size;
+			this->array = new T[arr.size];
+			for (unsigned int x = 0; x < arr.size; x++)
+				this->_array[x] = arr._array[x];
+		}
+		return *this;
+	}
+
+	T& operator[](unsigned int i)
+	{
+		if (i >= this->size)
+			throw OutOfRangeException();
+		return this->array[i];
+	}
+
+	unsigned int getSize()
+	{
+		return this->size;
+	}
+
+	class OutOfRangeException : public std::exception
+	{
+		public:
+			const char* what() const throw()
+			{
+				return ("Element is out of range of array");
+			}
+	};
+
+};
+
+#endif
